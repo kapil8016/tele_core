2d49517f870fdfa9586313f38b93f5d313de57ed
Kapil Rajpurohit <kapilrajajm@gmail.com>
2022-05-19 17:59:24 +0530

    first commit

---
+++ home/admin.py
@@ -0,0 +1,14 @@
+from email import feedparser
+from unicodedata import name
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+class CowinDataAdmin(admin.ModelAdmin):
+    list_display = [
+        "center_id","name","state","pincode","fee_type","fee"
+    ]
+
+admin.site.register(CowinData)
---
+++ telegrambot.py
@@ -0,0 +1,71 @@
+import django
+import os
+import re
+
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tele_core.settings'
+django.setup()
+
+
+from telegram.ext import *
+from home.models import *
+from home.helpers import *
+
+API_KEY = '5142443470:AAENhP6UQvikMYchlf0jtw1Z50qCg_kLra4'
+
+def isValidPinCode(pinCode):
+    regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+    p = re.compile(regex);
+    if (pinCode == ''):
+        return False;
+    m = re.match(p, pinCode);
+    if m is None:
+        return False
+    else:
+        return True
+
+def num_there(s):
+    return any(i.isdigit() for i in s)
+
+    
+
+
+def handle_message(update , context):
+    text = str(update.message.text).lower()
+
+    if num_there(text):
+        if isValidPinCode(text):
+            cowin_objs = CowinData.objects.filter(pincode = text)
+
+            if not cowin_objs.exists():
+                get_cowin_data_by_pincode(text)
+                cowin_objs = CowinData.objects.filter(pincode = text)
+
+            message = f""" Total {cowin_objs.count()} slots found in your pincode"""
+
+            for cowin_obj in cowin_objs:
+                message += f""" Place name {cowin_obj.fee_type} {cowin_obj.fee}{cowin_obj.available_capacity_dose1}{cowin_obj.available_capacity_dose2}{cowin_obj.available_capacity}{cowin_obj. minimum_age_limit}{cowin_obj.vaccine}\n \n"""
+
+            update.message.reply_text(f"{message}")
+            return
+        else:
+            update.message.reply_text(f"Not a valid pincode")
+            return 
+
+                 
+    
+
+    update.message.reply_text(f"Hi, {update['message']['chat']['first_name']} enter pincode to get vaccine updates")
+
+
+
+if __name__ == '__main__':
+    updater = Updater(API_KEY , use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(MessageHandler(Filters.text , handle_message ))
+
+    updater.start_polling(1.0)
+    updater.idle()
+
